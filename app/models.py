b276cc8c885c61e0f24b5e6b0c1e49b853bc5819
williamNyongesa <94893009+williamNyongesa@users.noreply.github.com>
2023-10-30 14:30:15 +0300

    add customer model (#2)
    Co-authored-by: robertnjunge <mubearobert@gmail.com>

---
+++ app/models.py
@@ -0,0 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy-serializer import SerializerMixin
+
+
+db=SQLAlchemy()
+
+class Customer(db.Model, SerializerMixin):
+    __tablename__='customers'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password_hash = db.Column(db.String)
+
+    def __repr__(self):
+        return f'name {self.username}'
