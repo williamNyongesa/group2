9f33320558ca95bccc6fd6b1a2ef5f1c3f485570
Medrine Mulindi <medrine.mulindi@gmail.com>
2023-11-05 14:48:58 +0300

    Implements Logout

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -1,6 +1,27 @@
+import { useNavigate } from "react-router-dom";
+import { useSnackbar } from "notistack";
 const Logout = () => {
+   const navigate = useNavigate()
+   const {enqueueSnackbar} = useSnackbar()
+
+
+const handleLogout = () =>{
+    fetch("/logout", {
+        method: "DELETE",
+        credentials: "include"
+    })
+    .then(r => {
+        if(r.status===200){
+            enqueueSnackbar("Log out  successfully!", {variant: "success"})
+        }else if (r.status===401){
+            enqueueSnackbar("Customer not logged in", {variant: "success"})
+            navigate("/login")
+        }
+    })
+    navigate("/")
+}
     return ( <div>
-        <h1>Logout Here</h1>
+        <button onClick={handleLogout}>Logout</button>
     </div> );
 }
  
